a5805eb7e33b3568c6747c7560011fa640166a15
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-07-07 16:34:55 -0600

    Created and designed "Navbar" component for mobile

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Navbar from "./Components/Navbar";
+
+import "./App.css";
+
+function App() {
+  return (
+    <section className="app-home">
+      <Navbar />
+    </section>
+  );
+}
+
+export default App;
