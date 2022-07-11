0ed3e56e70aada189febf66de999b31a42a31940
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-07-11 16:23:59 -0600

    Made and styled "Commitment" component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 
+import Commitment from "../Commitment/Commitment";
+
 import "./About.css";
 
 export default function About() {
   return (
-    <section className="about">
-      <h2 className="about__title">About us</h2>
-      <p className="about__description">
-        Coffeeroasters began its journey of exotic discovery in 1999,
-        highlighting stories of coffee from around the world. We have since been
-        dedicated to bring the perfect cup - from bean to brew - in every
-        shipment.
-      </p>
-    </section>
+    <>
+      <section className="about">
+        <h2 className="about__title">About us</h2>
+        <p className="about__description">
+          Coffeeroasters began its journey of exotic discovery in 1999,
+          highlighting stories of coffee from around the world. We have since
+          been dedicated to bring the perfect cup - from bean to brew - in every
+          shipment.
+        </p>
+      </section>
+
+      <Commitment />
+    </>
   );
 }
