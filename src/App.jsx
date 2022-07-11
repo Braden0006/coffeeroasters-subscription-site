1de923c7bf4b0e695999f4c1925cbfd0c09a8e93
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-07-11 15:39:08 -0600

    Made and designed "About" component and setup route for it

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./Components/Navbar/Navbar";
 import Homepage from "./Components/Homepage/Homepage";
-
 import Collection from "./Components/Collection/Collection";
-
 import WhyUs from "./Components/WhyUs/WhyUs";
-
 import HowItWorks from "./Components/HowItWorks/HowItWorks";
-
+import About from "./Components/About/About";
 import Footer from "./Components/Footer/Footer";
 
 import "./App.css";
@@ -34,6 +31,17 @@ function App() {
               </section>
               <section className="app-how">
                 <HowItWorks />
+              </section>
+            </>
+          }
+        ></Route>
+        <Route
+          path="/about"
+          element={
+            <>
+              <section className="app-about">
+                <Navbar />
+                <About />
               </section>
             </>
           }
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./About.css";
+
+export default function About() {
+  return (
+    <section className="about">
+      <h2 className="about__title">About us</h2>
+      <p className="about__description">
+        Coffeeroasters began its journey of exotic discovery in 1999,
+        highlighting stories of coffee from around the world. We have since been
+        dedicated to bring the perfect cup - from bean to brew - in every
+        shipment.
+      </p>
+    </section>
+  );
+}
