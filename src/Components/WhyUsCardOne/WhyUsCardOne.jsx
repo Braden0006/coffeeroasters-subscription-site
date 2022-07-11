ead9071705a4b3ee8edb27b0e3cfc0287bf45d3f
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-07-11 09:57:20 -0600

    Put the styles for the WhyUsCards in an seperate style folder

--- src/Components/WhyUsCardOne/WhyUsCardOne.jsx
+++ src/Components/WhyUsCardOne/WhyUsCardOne.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./WhyUsCardOne.css";
+import '../WhyUsCardStyle/WhyUsCardStyle.css';
 
 export default function WhyUsCardOne() {
   return (
--- src/Components/WhyUsCardThree/WhyUsCardThree.jsx
+++ src/Components/WhyUsCardThree/WhyUsCardThree.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "../WhyUsCardOne/WhyUsCardOne.css";
+import '../WhyUsCardStyle/WhyUsCardStyle.css';
 
 export default function WhyUsCardThree() {
   return (
