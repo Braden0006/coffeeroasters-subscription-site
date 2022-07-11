36d7d859dde3209e9456063d91ef083a7fba41a0
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-07-11 15:06:30 -0600

    Setup React Router in "main" and "app" components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./Components/Navbar/Navbar";
 import Homepage from "./Components/Homepage/Homepage";
@@ -9,29 +10,35 @@ import WhyUs from "./Components/WhyUs/WhyUs";
 
 import HowItWorks from "./Components/HowItWorks/HowItWorks";
 
-import Footer from './Components/Footer/Footer';
+import Footer from "./Components/Footer/Footer";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <section className="app-home">
-        <Navbar />
-        <Homepage />
-      </section>
-
-      <section className="app-collection">
-        <Collection />
-      </section>
-
-      <section className="app-why-us">
-        <WhyUs />
-      </section>
-
-      <section className="app-how">
-        <HowItWorks />
-      </section>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <section className="app-home">
+                <Navbar />
+                <Homepage />
+              </section>
+              <section className="app-collection">
+                <Collection />
+              </section>
+              <section className="app-why-us">
+                <WhyUs />
+              </section>
+              <section className="app-how">
+                <HowItWorks />
+              </section>
+            </>
+          }
+        ></Route>
+      </Routes>
 
       <section className="app-footer">
         <Footer />
