cf247ea3b8f4e8274ada8ca887ef63436459ac67
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-07-13 16:14:12 -0600

    When a choice card is clicked it adds styling to it

--- src/Components/Choices/Choices.jsx
+++ src/Components/Choices/Choices.jsx
@@ -1,30 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../ChoicesStyle/ChoicesStyle.css";
 
 export default function Choices(props) {
+  const [active, setActive] = useState("");
+
   return (
     <div className="choices">
-      <div className="choices__card choice-card-one">
-        <h2 className="choices__card__title">{props.one}</h2>
-        <p className="choices__card__info">
+      <div
+        className={
+          active === props.one ? "choices__card active" : "choices__card"
+        }
+        onClick={() => setActive(props.one)}
+      >
+        <h2
+          className={
+            active === props.one
+              ? "choices__card__title active-title"
+              : "choices__card__title"
+          }
+        >
+          {props.one}
+        </h2>
+        <p
+          className={
+            active === props.one
+              ? "choices__card__info active-info"
+              : "choices__card__info"
+          }
+        >
           {props.infoOne}
         </p>
       </div>
 
-      <div className="choices__card">
-        <h2 className="choices__card__title">{props.two}</h2>
-        <p className="choices__card__info">
+      <div
+        className={
+          active === props.two ? "choices__card active" : "choices__card"
+        }
+        onClick={() => setActive(props.two)}
+      >
+        <h2
+          className={
+            active === props.two
+              ? "choices__card__title active-title"
+              : "choices__card__title"
+          }
+        >
+          {props.two}
+        </h2>
+        <p
+          className={
+            active === props.two
+              ? "choices__card__info active-info"
+              : "choices__card__info"
+          }
+        >
           {props.infoTwo}
         </p>
       </div>
 
-      <div className="choices__card">
-        <h2 className="choices__card__title">{props.three}</h2>
-        <p className="choices__card__info">
+      <div
+        className={
+          active === props.three ? "choices__card active" : "choices__card"
+        }
+        onClick={() => setActive(props.three)}
+      >
+        <h2
+          className={
+            active === props.three
+              ? "choices__card__title active-title"
+              : "choices__card__title"
+          }
+        >
+          {props.three}
+        </h2>
+        <p
+          className={
+            active === props.three
+              ? "choices__card__info active-info"
+              : "choices__card__info"
+          }
+        >
           {props.infoThree}
         </p>
       </div>
     </div>
   );
+  console.log(active);
 }
