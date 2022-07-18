80aaf8f6394f7ae4da50f6aaa3ad157ae6c1e30c
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-07-17 18:05:26 -0600

    Put "Modal" component in a portal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import ReactDom from "react-dom";
 
 import "./Modal.css";
 
@@ -11,49 +12,52 @@ export default function Modal({ checkout }) {
     cardThreeActive,
     cardFourActive,
     cardFiveActive,
-    cardFivePrice
+    cardFivePrice,
   } = useContext(ChoiceCardContext);
 
-  if (checkout) {
-    return (
-      <section className="modal">
-        <div className="modal__content">
-          <div className="modal__content__title">
-            <h2 className="modal__content__title-title">Order Summary</h2>
-          </div>
-          <div className="modal__content__body">
-            <p className="modal__content__body__summary">
-              "I drink my coffee as{" "}
-              <span className="modal__content__body__summary-word">
-                {cardOneActive}
-              </span>
-              , with a{" "}
-              <span className="modal__content__body__summary-word">
-                {cardTwoActive}
-              </span>{" "}
-              type of bean.{" "}
-              <span className="modal__content__body__summary-word">
-                {cardThreeActive}
-              </span>{" "}
-              ground ala{" "}
-              <span className="modal__content__body__summary-word">
-                {cardFourActive}
-              </span>{" "}
-              sent to me every{" "}
-              <span className="modal__content__body__summary-word">
-                {cardFiveActive}
-              </span>
-              ."
-            </p>
-            <p className="modal__content__body__question">
-              Is this correct? You can proceed to checkout or go back to plan
-              selection if something is off. Subscription discount codes can
-              also be redeemed at the checkout.{" "}
-            </p>
-            <button className="modal__content__button">Checkout - {cardFivePrice} /mo</button>
-          </div>
+  if (!checkout) return null;
+
+  return ReactDom.createPortal(
+    <section className="modal">
+      <div className="modal__content">
+        <div className="modal__content__title">
+          <h2 className="modal__content__title-title">Order Summary</h2>
         </div>
-      </section>
-    );
-  }
+        <div className="modal__content__body">
+          <p className="modal__content__body__summary">
+            "I drink my coffee as{" "}
+            <span className="modal__content__body__summary-word">
+              {cardOneActive}
+            </span>
+            , with a{" "}
+            <span className="modal__content__body__summary-word">
+              {cardTwoActive}
+            </span>{" "}
+            type of bean.{" "}
+            <span className="modal__content__body__summary-word">
+              {cardThreeActive}
+            </span>{" "}
+            ground ala{" "}
+            <span className="modal__content__body__summary-word">
+              {cardFourActive}
+            </span>{" "}
+            sent to me every{" "}
+            <span className="modal__content__body__summary-word">
+              {cardFiveActive}
+            </span>
+            ."
+          </p>
+          <p className="modal__content__body__question">
+            Is this correct? You can proceed to checkout or go back to plan
+            selection if something is off. Subscription discount codes can also
+            be redeemed at the checkout.{" "}
+          </p>
+          <button className="modal__content__button">
+            Checkout - {cardFivePrice} /mo
+          </button>
+        </div>
+      </div>
+    </section>,
+    document.getElementById("portal")
+  );
 }
