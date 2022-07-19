a6cb0030f2a46bfc72d765d1960d006c4d3694d9
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-07-19 08:37:43 -0600

    Made "About" component responsive on tablet

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -10,13 +10,15 @@ export default function About() {
   return (
     <>
       <section className="about">
-        <h2 className="about__title">About us</h2>
-        <p className="about__description">
-          Coffeeroasters began its journey of exotic discovery in 1999,
-          highlighting stories of coffee from around the world. We have since
-          been dedicated to bring the perfect cup - from bean to brew - in every
-          shipment.
-        </p>
+        <div className="about__info-container">
+          <h2 className="about__info__title">About us</h2>
+          <p className="about__info__description">
+            Coffeeroasters began its journey of exotic discovery in 1999,
+            highlighting stories of coffee from around the world. We have since
+            been dedicated to bring the perfect cup - from bean to brew - in every
+            shipment.
+          </p>
+        </div>
       </section>
 
       <Commitment />
