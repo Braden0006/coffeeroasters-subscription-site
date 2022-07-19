8e8d2817ce66bae3ddf648ee160dd2968ff05999
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-07-19 10:29:31 -0600

    "Subscribe" is responsive on tablet

--- src/Components/Subscribe/Subscribe.jsx
+++ src/Components/Subscribe/Subscribe.jsx
@@ -22,12 +22,14 @@ export default function Subscribe() {
   return (
     <>
       <section className="subscribe">
-        <h2 className="subscribe__title">Create a plan</h2>
-        <p className="subscribe__description">
-          Build a subscription plan that best fits your needs. We offer an
-          assortment of the best artisan coffees from around the globe delivered
-          fresh to your door.
-        </p>
+        <div className="subscribe__info-container">
+          <h2 className="subscribe__info__title">Create a plan</h2>
+          <p className="subscribe__info__description">
+            Build a subscription plan that best fits your needs. We offer an
+            assortment of the best artisan coffees from around the globe delivered
+            fresh to your door.
+          </p>
+        </div>
       </section>
 
       <HowItWorksTwo />
